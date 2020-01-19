d17e846b721bb1cb307837655a5f0af82eff35c0
Mandy Cheang <mandycheang135@gmail.com>
2020-01-19 16:01:07 -0500

    add url

--- 7-kyu-find-the-next-perfect-square.rb
+++ 7-kyu-find-the-next-perfect-square.rb
@@ -1,4 +1,5 @@
 =begin
+https://www.codewars.com/kata/56269eb78ad2e4ced1000013
 You might know some pretty large perfect squares. But what about the NEXT one?
 
 Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
